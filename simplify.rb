9abac6919feef3845846f6e29f476f222b43100d
Ian McGaunn <ianmcgaunn@Ians-MacBook-Air.local>
2013-04-12 10:58:56 -0400

    added the basic methods

--- simplify.rb
+++ simplify.rb
@@ -8,4 +8,40 @@ def getEntries( url )
   entries = feed.entries
 end
 
+def startTemplate()
+  # insert beginning of html template
+  document = File.open("simplify.html", "w")
+
+  File.open('temp1.html').each do |line|
+    document << line
+  end
+
+  document.close
+end
+
+def endTemplate()
+  document = File.open( "simplify.html", "w")
+
+  File.open('temp2.html').each do |line|
+    document << line
+  end
+
+  document.close
+end
+
+def genPage( entries )
+  # create a div for each entry content
+
+  startTemplate()
+
+  entries.each do |entry|
+    print entry.content
+  end
+
+  #  endTemplate()
+
+end
+
+entries = getEntries("http://thissongissick.com/blog/feed/")
+genPage( entries )
 # take entries and generate page
